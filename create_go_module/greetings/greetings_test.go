b44e6c34b0b3c44954243fe92eb4dfeea5f77cb5
gcode <net.git.ketza@gmail.com>
2023-03-14 22:19:52 -0600

    Add a test greetings_test.go Changes:   * Implement test functions in the same package as the code you're testing.   * Create two test functions to test the greetings.Hello function. Test function names have the form TestName, where Name says something about the specific test. Also, test functions take a pointer to the testing package's testing.T type as a parameter. You use this parameter's methods for reporting and logging from your test.   * Implement two tests:     * TestHelloName calls the Hello function, passing a name value with which the function should be able to return a valid response message. If the call returns an error or an unexpected response message (one that doesn't include the name you passed in), you use the t parameter's Fatalf method to print a message to the console and end execution.     * TestHelloEmpty calls the Hello function with an empty string. This test is designed to confirm that your error handling works. If the call returns a non-empty string or no error, you use the t parameter's Fatalf method to print a message to the console and end execution. use $ go test -v to view test

---
+++ create_go_module/greetings/greetings_test.go
@@ -0,0 +1,27 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want for %#q, nil`, msg, err, want)
+	}
+}
+
+// TestHelloEmpty calls greeting.Hello with and empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
+
