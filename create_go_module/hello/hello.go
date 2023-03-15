aff936f604dd6133695ea4d1b93ee27fe3a228c3
gcode <net.git.ketza@gmail.com>
2023-03-14 21:45:55 -0600

    Return greetings for multiple people greetings.go file Changes:   * Add a Hellos function whose parameters is a slice of names rather than a single name. Also, you change one of its return types from a string to a map so you can return names mapped to greeting messages.   * Have the new Hellos function call the existing Hello function. This helps reduce duplication while also leaving both functions in place.   * Create a messages map to associate each of the received names (as a key) with a generated message (as a value).   * Loop through the names your function received, checking that each has a non-empty value, then associate a message with each. In this for loop, range return two values: the index of the current item in the loop and a copy of the item's value. You don't need the index, so you use the Go blank identifier (an underscore) to ignore it. hello.go file Changes:   * Create a names variable as a slice type holding three names.   * Pass the names variable as the argument to the Hellos function.

--- create_go_module/hello/hello.go
+++ create_go_module/hello/hello.go
@@ -14,15 +14,18 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Pepe")
+	// A slices of names.
+	names := []string{"Gladys's", "Samantha", "Darrin"}
+
+	// Request a greeting messages for the names.
+	// message, err := greetings.Hello("Pepe")
+	message, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	
 	// If not error was returned, print returned message
 	// to the console
 
